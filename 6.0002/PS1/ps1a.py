abbe5780ad8e6c5d5efbc4821af90b6b56123733
reiogo <raogoda2@gmail.com>
2024-04-24 13:51:40 +0900

    New files, and updates

--- 6.0002/PS1/ps1a.py
+++ 6.0002/PS1/ps1a.py
@@ -1,6 +1,6 @@
 ###########################
 # 6.0002 Problem Set 1a: Space Cows 
-# Name:
+# Name: Ray O
 # Collaborators:
 # Time:
 
@@ -24,8 +24,20 @@ def load_cows(filename):
     Returns:
     a dictionary of cow name (string), weight (int) pairs
     """
-    # TODO: Your code here
-    pass
+    # Create dictionary.
+    cow_dict = {}
+    # Open and file.
+    with open(filename, encoding = "utf-8") as f:
+        # Loop over the file by line.
+        for line in f:
+            # Remove the newline \n.
+            line = line[:-1]
+            # Split the lines into name and weight.
+            tmp = line.split(",")
+            # Add to the dictionary (make the weight an int).
+            cow_dict[tmp[0]] = int(tmp[1])
+    return cow_dict
+
 
 # Problem 2
 def greedy_cow_transport(cows,limit=10):
@@ -50,8 +62,36 @@ def greedy_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    # TODO: Your code here
-    pass
+    # Initialize variables.
+    cargo_weight = 0
+    cargo = []
+    res = []
+    # Create a copy of cows dict.
+    copy_cows_dict = cows.copy()
+    # Loop until all cows are in the results.
+    while copy_cows_dict != {}:
+        # Create a sorted list from cows dictionary.
+        copy_cows = sorted(copy_cows_dict.items(), key=lambda x:x[1], reverse=True)
+        # Loop over sorted list.
+        for i in range(len(copy_cows)):
+            # If the weight doesn't exceed 10,
+            if cargo_weight+(copy_cows[i])[1] <= limit:
+                # Add cow at index i to the cargo
+                cargo.append((copy_cows[i])[0])
+                cargo_weight += (copy_cows[i])[1]
+            else:
+                break
+        # Add result of for loop to results.
+        res.append(cargo)
+        # Delete cows that were already used once. 
+        for c in cargo:
+            if c in copy_cows_dict:
+                del copy_cows_dict[c]
+        # Reset cargo and weight.
+        cargo = []
+        cargo_weight = 0
+    return res
+
 
 # Problem 3
 def brute_force_cow_transport(cows,limit=10):
@@ -75,9 +115,33 @@ def brute_force_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    # TODO: Your code here
-    pass
-        
+    # Initiate the best set(the highest number of trips).
+    bestSet = cows
+    # Initiate flag to check if cargo weight is exceeded.
+    flag = True
+    # For loop over each partition.
+    for partition in get_partitions(cows):
+        # For loop over each trip.
+        for cargo in partition:
+            # Reset cargo weight to zero.
+            cargo_weight = 0
+            # For loop over each cow.
+            for cow in cargo:
+                # Append to cargo weight the cows weight.
+                cargo_weight+=cows.get(cow)
+                # If cargo weight exceeds the limit, set flag to false.
+                if cargo_weight > limit:
+                    flag = False
+                    break
+        # If flag is true, and the trip number is better than the best so far
+        # set the best to the new best.
+        if flag and len(partition) < len(bestSet):
+            bestSet = partition
+        # Reset flag.
+        flag = True
+    return(bestSet)
+
+
 # Problem 4
 def compare_cow_transport_algorithms():
     """
@@ -92,5 +156,25 @@ def compare_cow_transport_algorithms():
     Returns:
     Does not return anything.
     """
-    # TODO: Your code here
-    pass
+    data = 'ps1_cow_data_2.txt'
+
+    start = time.time()
+    ## code to be timed
+    x = greedy_cow_transport(load_cows(data),limit=10)
+    print(x)
+    end = time.time()
+    print ('greedy: ',end - start)
+
+
+    start = time.time()
+    ## code to be timed
+    m = brute_force_cow_transport(load_cows(data))
+    print(m)
+    end = time.time()
+    print ('brute: ', end - start)
+    
+compare_cow_transport_algorithms()
+
+
+
+
